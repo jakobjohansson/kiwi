febe3840d28ab4e4a439139427d0a0737b72eaa6
Jakob Johansson <jakobjohansson@Jakob-Mac.local>
2017-02-10 12:50:38 +0100

    shortcode public variable

--- admin/lib/overview.php
+++ admin/lib/overview.php
@@ -11,10 +11,7 @@ if (!auth()) {
     <div class="col-xs-12 col-sm-10 col-md-8">
         <?php
         while ($vb->loopPosts(true)) {
-            $public = '<i class="fa fa-shield" aria-hidden="true"></i>';
-            if ($post->public == 1) {
-                $public = null;
-            }
+            $public = ($post->public == 0) ? '<i class="fa fa-shield" aria-hidden="true"></i>' : null;
             echo "<article><h3>$post->title <small>$post->date $public</small></h3>";
             echo "$post->excerpt";
             echo "<div class='links'><a href='../?id=$post->id'>Permalink</a> <a href='#' class='editor' data-id='$post->id'>Edit</a> <a href='?page=remove&id=$post->id' class='post-deleter'>Trash</a></div></article>";
