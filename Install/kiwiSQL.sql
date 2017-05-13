6db7094f5f668fdff7f96a959bdda056ed7220e1
jakobjohansson <jakobjohansson2@icloud.com>
2017-05-13 16:01:36 +0200

    generate sql structure and default entries

---
+++ Install/kiwiSQL.sql
@@ -0,0 +1,108 @@
+
+# Dump of table site_meta
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `site_meta`;
+
+CREATE TABLE `site_meta` (
+  `meta_id` int(11) NOT NULL AUTO_INCREMENT,
+  `key` varchar(255) NOT NULL DEFAULT '',
+  `value` varchar(255) NOT NULL DEFAULT '',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`meta_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8 COMMENT='Meta data for the site, such as what theme to use, name, description, public etc.';
+
+LOCK TABLES `site_meta` WRITE;
+/*!40000 ALTER TABLE `site_meta` DISABLE KEYS */;
+
+INSERT INTO `site_meta` (`meta_id`, `key`, `value`)
+VALUES
+    (1,'name','kiwi'),
+    (2,'description','an optional description of the site'),
+    (3,'public','true'),
+    (4,'theme','kiwi-default');
+
+/*!40000 ALTER TABLE `site_meta` ENABLE KEYS */;
+UNLOCK TABLES;
+
+# Dump of table users
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `password` varchar(32) NOT NULL,
+  `avatar` varchar(45) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` int(11) NOT NULL DEFAULT '1' COMMENT '1: user,\n2: contributor,\n3: moderator,\n4: admin',
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Users table. The higher role number the higher privileges.';
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `avatar`, `role`)
+VALUES
+    (1,'admin','admin@admin.com','password',NULL,4);
+
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+# Dump of table types
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `types`;
+
+CREATE TABLE `types` (
+  `type_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  PRIMARY KEY (`type_id`),
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COMMENT='Registered types of items. Defaults to post, project and comment.';
+
+LOCK TABLES `types` WRITE;
+/*!40000 ALTER TABLE `types` DISABLE KEYS */;
+
+INSERT INTO `types` (`type_id`, `name`)
+VALUES
+    (3,'comment'),
+    (1,'post'),
+    (2,'project');
+
+/*!40000 ALTER TABLE `types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `items`;
+
+CREATE TABLE `items` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `type_id` int(11) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `body` text,
+  `public` tinyint(4) DEFAULT '1',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `belongs_to_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`item_id`),
+  KEY `user_id_idx` (`user_id`),
+  KEY `type_id_idx` (`type_id`),
+  CONSTRAINT `type_id` FOREIGN KEY (`type_id`) REFERENCES `types` (`type_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Contains all items on the site.';
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+
+INSERT INTO `items` (`item_id`, `type_id`, `user_id`, `title`, `body`, `belongs_to_id`)
+VALUES
+	(1,1,1,'This is an example post','Welcome to kiwi',NULL);
+
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
