0126ee28a5f823309a7365e91b7b98855bb067d3
jakobjohansson <jakobjohansson2@icloud.com>
2017-05-13 20:30:05 +0200

    rearrange folders
    since kiwiworks will be used again, put it in a root stock folder

--- install/index.view.php
+++ install/index.view.php
@@ -10,8 +10,6 @@
     <div class="container full dark text-center">
         <div class="col center full">
             <h1>kiwi</h1>
-            <p>Trying out kiwi? Good choice. We just need a few minutes of your time to configurate the install.</p>
-            <a href="started.html" class="button">Get started</a>
         </div>
     </div>
 </body>
