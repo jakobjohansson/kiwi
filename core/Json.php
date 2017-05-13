06c579c49e2fb048d4a4a430ec32c676c57b9a60
jakobjohansson <jakobjohansson2@icloud.com>
2017-05-14 00:36:33 +0200

    input and json classes for returning sanitized post values

---
+++ core/Json.php
@@ -0,0 +1,37 @@
+<?php namespace kiwi;
+
+class Json implements InputInterface
+{
+
+    /**
+     * Returns a sanitized JSON field
+     *
+     * @param  string $key the input field
+     * @return string
+     */
+    public static function field($key)
+    {
+        $json = json_decode(file_get_contents('php://input'), true);
+        return Sanitizer::input($json[$key]);
+    }
+
+
+    /**
+     * Sanitize all the fields in a JSON collection
+     *
+     * @param  string $type JSON
+     * @return array        Sanitized array.
+     */
+    public static function all()
+    {
+        $json = json_decode(file_get_contents('php://input'), true);
+        $sendback = [];
+
+        foreach ($json as $key => $value) {
+            $sendback[$key] = Sanitizer::input($json[$key]);
+        }
+
+
+        return $sendback;
+    }
+}
