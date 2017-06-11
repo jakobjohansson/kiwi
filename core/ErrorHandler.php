670ff8257bf4c0dcf72aa19360c0abd9082b0e73
Jakob Johansson <jakobjohansson2@icloud.com>
2017-06-11 11:24:13 +0200

    handle with grace

--- core/ErrorHandler.php
+++ core/ErrorHandler.php
@@ -9,13 +9,16 @@ class ErrorHandler
      * Render the error page.
      *
      * @param  Exception $e
+     * @param Query $query
      */
-    public static function renderErrorView(Exception $e, Query $query) {
+    public static function renderErrorView(Exception $e, Query $query = null) {
 
-        $customErrorFile = 'themes/' . Config::get('theme', $query) . '/404.view.php';
+        if ($query) {
+            $customErrorFile = 'themes/' . Config::get('theme', $query) . '/404.view.php';
 
-        if (file_exists($customErrorFile)) {
-            return require $customErrorFile;
+            if (file_exists($customErrorFile)) {
+                return require $customErrorFile;
+            }
         }
 
         return require 'themes/kiwi-default/404.view.php';
