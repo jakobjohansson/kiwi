cf246071b91c967a0ad78e4d8251470a8fbc95d5
Jakob Johansson <jakobjohansson2@icloud.com>
2017-06-11 09:24:18 +0000

    Apply fixes from StyleCI

--- core/ErrorHandler.php
+++ core/ErrorHandler.php
@@ -4,17 +4,16 @@ namespace kiwi;
 
 class ErrorHandler
 {
-
     /**
      * Render the error page.
      *
-     * @param  Exception $e
-     * @param Query $query
+     * @param Exception $e
+     * @param Query     $query
      */
-    public static function renderErrorView(Exception $e, Query $query = null) {
-
+    public static function renderErrorView(Exception $e, Query $query = null)
+    {
         if ($query) {
-            $customErrorFile = 'themes/' . Config::get('theme', $query) . '/404.view.php';
+            $customErrorFile = 'themes/'.Config::get('theme', $query).'/404.view.php';
 
             if (file_exists($customErrorFile)) {
                 return require $customErrorFile;
