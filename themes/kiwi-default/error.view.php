8ec90fb94d8b2c4989cd12458b83e65e0f124240
Jakob Johansson <jakobjohansson2@icloud.com>
2017-06-11 13:35:16 +0200

    red error page, folder up some

--- themes/kiwi-default/error.view.php
+++ themes/kiwi-default/error.view.php
@@ -1,3 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
+    <title>kiwi - woops</title>
+</head>
+<body>
 
-echo $exception->getMessage();
+    <section class="hero is-bold is-danger is-fullheight">
+        <div class="hero-body">
+            <div class="container">
+                <div class="columns">
+                    <div class="column is-half is-offset-one-quarter">
+                        <h1 class="title">Woops!</h1>
+                        <p class="field">Something went wrong. The server sent the following message:</p>
+                        <p class="field"><?=$exception->getMessage();?></p>
+                        <p class="field">
+                            <a href="javascript:history.back();" class="button is-info">Go back</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+</body>
+</html>
---
+++ themes/kiwi-default/partials/head.view.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
+    <title>kiwi</title>
+</head>
+<body>
