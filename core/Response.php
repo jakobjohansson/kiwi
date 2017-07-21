a10905a765f3bc94a3c91c264bb4885a9312b5ec
Jakob Johansson <jakobjohansson2@icloud.com>
2017-07-21 17:33:13 +0900

    add a response class

---
+++ core/Response.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace kiwi;
+
+class Response
+{
+    /**
+     * Return a json response.
+     * @method json
+     * @param  mixed $payload
+     * @return Json
+     */
+    public function json($payload)
+    {
+        echo Json::make($payload);
+        exit;
+    }
+
+    /**
+     * Return a status code.
+     * @method status
+     * @param  integer $code
+     * @return Response
+     */
+    public function status($code)
+    {
+        // TODO: Return a status code and exit the application.
+    }
+}
