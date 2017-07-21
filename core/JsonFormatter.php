97490342caff3b15c6a51fcb2a4bb435d29624cb
Jakob Johansson <jakobjohansson2@icloud.com>
2017-07-21 17:24:22 +0900

    some more documentation

--- core/JsonFormatter.php
+++ core/JsonFormatter.php
@@ -4,11 +4,23 @@ namespace kiwi;
 
 class JsonFormatter implements FormatterInterface
 {
+    /**
+     * Turn a resource into JSON.
+     * @method make
+     * @param  mixed $value
+     * @return mixed
+     */
     public static function make($value)
     {
         return json_encode($value);
     }
 
+    /**
+     * Translate JSON into a readable resource.
+     * @method get
+     * @param  mixed $value
+     * @return mixed
+     */
     public static function get($value)
     {
         return json_decode($value);
