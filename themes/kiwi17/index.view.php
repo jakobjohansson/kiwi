9bb63f1af11293a4d368cd7da959de9500630bb8
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-19 20:42:27 +0200

    rename default theme and remove partials

--- themes/kiwi17/index.view.php
+++ themes/kiwi17/index.view.php
@@ -1,6 +1,12 @@
-<?php
-require 'partials/head.view.php';
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
+    <title>kiwi</title>
+</head>
+<body>
 
 <section class="section">
     <div class="container">
@@ -45,6 +51,5 @@ require 'partials/head.view.php';
         </div>
     </div>
 </section>
-
-<?php
-require 'partials/foot.view.php';
+</body>
+</html>
