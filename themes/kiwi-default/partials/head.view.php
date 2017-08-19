dfafeb00be32ca562c5453b1a2f97f53e079beed
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-19 20:38:18 +0200

    remove local stylesheet links

--- themes/kiwi-default/partials/head.view.php
+++ themes/kiwi-default/partials/head.view.php
@@ -4,7 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.4.2/css/bulma.min.css">
-    <link rel="stylesheet" href="bulma.css" />
     <title>kiwi</title>
 </head>
 <body>
