6f29ac6379f0b6ba35cb52652ef7e22b915d2af8
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-20 11:37:48 +0000

    Apply fixes from StyleCI

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
 use kiwi\Database\Connection;
-use kiwi\System\Filesystem;
 use kiwi\Http\Model;
+use kiwi\System\Filesystem;
 
-if (!Filesystem::find('Custom' . DIRECTORY_SEPARATOR . 'config.php')) {
+if (!Filesystem::find('Custom'.DIRECTORY_SEPARATOR.'config.php')) {
     return;
 }
 
-Model::boot(Connection::make(require 'Custom' . DIRECTORY_SEPARATOR . 'config.php'));
+Model::boot(Connection::make(require 'Custom'.DIRECTORY_SEPARATOR.'config.php'));
