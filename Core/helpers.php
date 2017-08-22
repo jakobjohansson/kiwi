7018e72df293fdee4118aa70104d2fcc879fd8d6
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-22 21:02:42 +0200

    add dd() to helpers

---
+++ Core/helpers.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * kiwi helpers file.
+ */
+
+if (!function_exists('dd')) {
+    function dd($param)
+    {
+        echo '<pre>';
+        var_dump($param);
+        echo '</pre>';
+        die();
+    }
+}
