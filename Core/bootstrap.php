ffaab808ce24fe25d94db7052bf3c3b68713106a
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-22 21:08:05 +0200

    move index bootstrapping to bootstrap file

--- Core/bootstrap.php
+++ Core/bootstrap.php
@@ -2,10 +2,24 @@
 
 use kiwi\Database\Connection;
 use kiwi\Database\Model;
+use kiwi\Http\Router;
+use kiwi\Error\ErrorHandler;
 use kiwi\System\Filesystem;
 
 if (!Filesystem::find('App'.DIRECTORY_SEPARATOR.'config.php')) {
     return;
 }
 
-Model::boot(Connection::make(require 'App'.DIRECTORY_SEPARATOR.'config.php'));
+Model::boot(
+    Connection::make(
+        require 'App'.DIRECTORY_SEPARATOR.'config.php'
+    )
+);
+
+try {
+    Router::loadRoutes(
+        'App'.DIRECTORY_SEPARATOR.'routes.php'
+    )->delegate();
+} catch (Exception $exception) {
+    return ErrorHandler::renderErrorView($exception);
+}
--- index.php
+++ index.php
@@ -1,10 +1,3 @@
 <?php
 
-// use composer in development mode
 require 'vendor'.DIRECTORY_SEPARATOR.'autoload.php';
-
-try {
-    kiwi\Http\Router::loadRoutes('App'.DIRECTORY_SEPARATOR.'routes.php')->delegate();
-} catch (Exception $exception) {
-    return kiwi\Error\ErrorHandler::renderErrorView($exception);
-}
