42f3207e5c4acf14fce4bdef1edd8555b117e23b
Jakob Johansson <jakobjohansson2@icloud.com>
2017-08-26 12:34:59 +0200

    add a DI container

---
+++ Core/Container.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace kiwi;
+
+class Container
+{
+    /**
+     * Value registry.
+     * @var Array
+     */
+    private static $registry = [];
+
+    /**
+     * Bind a value to the container.
+     * @param  string $key
+     * @param  mixed $value
+     * @return void
+     */
+    public static function bind($key, $value)
+    {
+        static::$registry[$key] = $value;
+    }
+
+    /**
+     * Resolve a value out of the container.
+     * @param  string $key
+     * @return mixed
+     */
+    public static function resolve($key)
+    {
+        return static::$registry[$key];
+    }
+}
