37bb51d1e465e008af5be7273776b3d805f5bbe6
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-04 23:47:32 +0200

    implement arrayAccess in model

--- Core/Database/Model.php
+++ Core/Database/Model.php
@@ -4,11 +4,37 @@ namespace kiwi\Database;
 
 use kiwi\Container;
 
-abstract class Model
+abstract class Model implements \ArrayAccess
 {
+
+    public $attributes = [];
 
     public static function builder()
     {
         return new Builder(Container::resolve('connection'));
+    }
+
+    public function offsetExists($offset)
+    {
+        return in_array($this->attributes, $offset);
+    }
+
+    public function offsetGet($offset)
+    {
+        return $this->attributes[$offset] ?? null;
+    }
+
+    public function offsetSet($offset, $value)
+    {
+        if (is_null($offset)) {
+            $this->attributes[] = $value;
+        } else {
+            $this->attributes[$offset] = $value;
+        }
+    }
+
+    public function offsetUnset($offset)
+    {
+        unset($this->attributes[$offset]);
     }
 }
