c782bcc19b2bf4ff2a4d3509192ac5fa2fb0c04b
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-05 16:23:19 +0200

    add a view for managing users in admin interface

---
+++ Core/Admin/views/users.view.php
@@ -0,0 +1,34 @@
+<?php require __DIR__.'/partials/admin-header.php'; ?>
+
+<div class="column is-offset-3 is-6">
+    <table class="table">
+        <thead>
+            <tr>
+                <th>
+                    Username
+                </th>
+                <th>
+                    Email
+                </th>
+                <th>
+                    Role
+                </th>
+                <th>
+                    Created at
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            foreach ($users as $user) {
+                echo "<td>{$user->username}</td>";
+                echo "<td>{$user->email}</td>";
+                echo "<td>{$user->role}</td>";
+                echo "<td>{$user->created_at}</td>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
+
+<?php require __DIR__.'/partials/admin-footer.php'; ?>
