2bf1be735b17df537ffc6e68ae62fd5553c41f69
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-05 16:11:12 +0200

    echo username

--- Core/Admin/views/index.view.php
+++ Core/Admin/views/index.view.php
@@ -5,7 +5,7 @@
     foreach ($posts as $post) {
         ?>
         <h2 class="title"><?php echo $post->title?></h2>
-        <h3 class="subtitle">Written by Jakob</h3>
+        <h3 class="subtitle">Written by <?php echo $post->username ?></h3>
         <div class="content">
             <?php echo $post->body ?>
         </div>
