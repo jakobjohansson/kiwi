7f92a56544a7ec4c40daa08a8feb8bdec519101f
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 00:03:17 +0200

    remove installer entirely, placeholder migration class

---
+++ Core/Migration.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace kiwi;
+
+class Migration
+{
+    private $migrationsPath;
+
+    private function refreshMigration()
+    {
+        
+    }
+
+    private function migrateTables()
+    {
+        $this->pdo->query(require static::$migs.'site_meta.php');
+        $this->pdo->query(require static::$migs.'users.php');
+        $this->pdo->query(require static::$migs.'types.php');
+        $this->pdo->query(require static::$migs.'items.php');
+    }
+
+    private function seedTables()
+    {
+        $this->pdo->query(require static::$seeds.'site_meta.php');
+        $this->pdo->query(require static::$seeds.'types.php');
+
+        // TODO: Do admin user here.
+
+        $this->pdo->query(require static::$seeds.'items.php');
+    }
+}
