e1e607e0ea9f8aa9e1ef95b9a7bd877b5909e005
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 00:22:01 +0200

    refactor view

--- Core/Http/View.php
+++ Core/Http/View.php
@@ -2,9 +2,6 @@
 
 namespace kiwi\Http;
 
-use kiwi\Container;
-use kiwi\System\Filesystem;
-
 class View
 {
     /**
@@ -17,13 +14,9 @@ class View
      */
     public static function render($view, array $extracts = [])
     {
-        $app = Container::resolve('app');
-
         extract($extracts, EXTR_SKIP);
 
-        return require 'App'.DIRECTORY_SEPARATOR.
-            'Themes'.DIRECTORY_SEPARATOR."{$app->theme}".
-            DIRECTORY_SEPARATOR."{$view}.view.php";
+        require 'App'.DIRECTORY_SEPARATOR."Views".DIRECTORY_SEPARATOR."{$view}.view.php";
     }
 
     /**
@@ -36,20 +29,8 @@ class View
      */
     public static function renderCustom($path, array $extracts = [])
     {
-        $app = Container::resolve('app');
-
         extract($extracts, EXTR_SKIP);
 
-        return require "$path";
-    }
-
-    /**
-     * Retrieve the available themes.
-     *
-     * @return array
-     */
-    public static function getThemeLinks()
-    {
-        return Filesystem::scanFolder('App'.DIRECTORY_SEPARATOR.'Themes');
+        require "$path";
     }
 }
