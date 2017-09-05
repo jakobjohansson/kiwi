df21a28844d3d986262e2adebc00c0dcc3769c29
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-05 23:58:24 +0200

    remove installer and do some folder restructuring

--- Install/views/index.view.php
+++
@@ -1,2 +0,0 @@
-<p class="field">Trying out kiwi? Good choice. We just need a few minutes of your time to configurate the install.</p>
-<p class="field"><a href="/database" class="button is-info">Get started</a></p>
--- Install/views/partials/foot.view.php
+++
@@ -1,6 +0,0 @@
-            </div>
-        </div>
-    </section>
-    <script src="/install/assets/installer.js"></script>
-</body>
-</html>
