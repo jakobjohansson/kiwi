fb05dcfea4a0285133cac16ea945939ab3ab00e9
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 12:55:20 +0200

    remove user page

--- Core/Admin/AdminController.php
+++ Core/Admin/AdminController.php
@@ -33,32 +33,12 @@ class AdminController extends Controller
     }
 
     /**
-     * Render the users page.
-     *
-     * @return void
-     */
-    public function users()
-    {
-        View::renderAdminView(
-            'users',
-            [
-                'users' => User::all(),
-            ]
-        );
-    }
-
-    /**
      * Render the options page.
      *
      * @return void
      */
     public function options()
     {
-        View::renderAdminView(
-            'options',
-            [
-                'themes' => View::getThemeLinks(),
-            ]
-        );
+        View::renderAdminView('options');
     }
 }
