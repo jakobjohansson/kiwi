2ac84805e5fd0723480a9e25f0d4dcec455fcbbb
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 11:52:45 +0200

    put a default migrate route

--- App/routes.php
+++ App/routes.php
@@ -4,4 +4,8 @@
  * You can create your own custom routes in here, at the end of this file.
  */
 
+ $router->get('migrate', function () {
+     Migrate::refresh();
+ });
+
 $router->get('', 'PageController/index');
