42cf1a2c1fd381368c6e7fafddc81a59024d97f1
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 15:50:48 +0200

    warning

--- App/routes.php
+++ App/routes.php
@@ -4,6 +4,7 @@
  * You can create your own custom routes in here, at the end of this file.
  */
 
+// Warning: This route should not be used in production.
  $router->get('migrate', function () {
      kiwi\Database\Migration::refresh();
  });
