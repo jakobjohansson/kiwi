30ce8222b9bfda8d7e0c51f2107c20e7bd8c392e
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-06 12:49:49 +0200

    app docs

--- Core/Application.php
+++ Core/Application.php
@@ -4,10 +4,26 @@ namespace kiwi;
 
 class Application
 {
+    /**
+     * The application name.
+     *
+     * @var string
+     */
     public $name;
 
+    /**
+     * The application description.
+     *
+     * @var string
+     */
     public $description;
-    
+
+    /**
+     * Create a new Application instance.
+     *
+     * @param string $name
+     * @param string $description
+     */
     public function __construct($name, $description)
     {
         $this->name = $name;
