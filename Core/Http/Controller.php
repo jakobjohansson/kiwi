5285294e2d0a3eaf7450fbcc2202cbcdc867fac1
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-07 15:33:30 +0200

    update controller

--- Core/Http/Controller.php
+++ Core/Http/Controller.php
@@ -4,4 +4,10 @@ namespace kiwi\Http;
 
 abstract class Controller
 {
+    public function __construct()
+    {
+        if (method_exists($this, 'middleware')) {
+            $this->middleware();
+        }
+    }
 }
