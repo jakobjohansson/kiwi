57442442d448e3090b9f517f9082ce31a512a7ea
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-12 15:45:21 +0200

    set some error messages from posting

--- Core/Admin/AdminController.php
+++ Core/Admin/AdminController.php
@@ -52,8 +52,21 @@ class AdminController extends Controller
     {
         $post = new Post();
 
-        $post->title = Input::field('title');
-        $post->body = Input::field('body');
+        $post->title = Input::field(
+            'title',
+            [
+                'required' => "The title is required.",
+                'min:3' => "The title need to be atleast 3 characters long."
+            ]
+        );
+
+        $post->body = Input::field(
+            'body',
+            [
+                'required' => "The body field is required.",
+                'min:5' => "The body needs to be atleast 5 characters long."
+            ]
+        );
 
         $post->save();
 
