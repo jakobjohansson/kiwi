8d8d121ed6e83362b7a6f63c6912a6cec6f2aa04
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-12 17:02:39 +0200

    todo

--- Core/Http/Request.php
+++ Core/Http/Request.php
@@ -47,6 +47,7 @@ class Request
      */
     public static function back()
     {
+        // TODO: send back values aswell.
         resolve('session')->set('errors', resolve('bag')->errors);
 
         self::redirect('/'.self::uri());
