e16ecfb5f0b88390155c81702ff632a839134607
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-13 21:02:05 +0200

    set empty string to null from pdo

--- Core/Database/Connection.php
+++ Core/Database/Connection.php
@@ -20,7 +20,10 @@ class Connection
                 env('DATABASE_HOST') . ';dbname=' . env('DATABASE_NAME'),
                 env('DATABASE_USER'),
                 env('DATABASE_PASS'),
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_ORACLE_NULLS => PDO::NULL_EMPTY_STRING
+                ]
             );
         } catch (PDOException $e) {
             throw $e;
