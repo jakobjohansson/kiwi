3daf7e2a880945434bd6d726c3b67f5a92273a9b
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-15 00:54:15 +0200

    some refactoring and docs for the injector

--- Core/Injector.php
+++ Core/Injector.php
@@ -4,37 +4,133 @@ namespace kiwi;
 
 class Injector
 {
+    /**
+     * The reflected class.
+     *
+     * @var ReflectionClass
+     */
     private $class;
 
+    /**
+     * The reflected method.
+     *
+     * @var ReflectionMethod
+     */
     private $method;
 
-    private $parameters;
+    /**
+     * The reflected parameter.
+     *
+     * @var ReflectionParameter
+     */
+    private $parameter;
 
+    /**
+     * The reflected type.
+     *
+     * @var ReflectionType
+     */
     private $type;
 
+    /**
+     * The original parameter.
+     *
+     * @var string
+     */
     private $httpParameters;
 
+    /**
+     * Create a new Injector instance.
+     *
+     * @param string $class
+     * @param string $methodName
+     * @param string $httpParameters
+     */
     public function __construct($class, $methodName, $httpParameters)
     {
         $this->httpParameters = $httpParameters;
-        $this->reflection = new \ReflectionClass(new $class());
 
-        $methods = $this->reflection->getMethods();
+        $this->setReflectionClass($class)->setReflectionMethod($methodName)->setReflectionParameter();
 
-        array_walk($methods, function ($method) use ($methodName) {
-            if ($method->name === $methodName) {
-                $this->method = $method;
-                $this->parameters = $method->getParameters();
-            }
-        });
+        if (!count($this->parameters)) {
+            return false;
+        }
 
-        foreach ($this->parameters as $parameter) {
-            if ($parameter->hasType()) {
-                $this->type = $parameter->getType()->__toString();
-            }
+        $this->setParameterType();
+
+        if (!is_null($this->type) && $this->typeIsBuiltIn()) {
+            $type = $type->__toString();
         }
     }
 
+    /**
+     * Check if the type is built in.
+     *
+     * @return bool
+     */
+    private function typeIsBuiltIn()
+    {
+        return $this->type->isBuiltin();
+    }
+
+    /**
+     * Set the parameter type.
+     *
+     * @return void
+     */
+    private function setParameterType()
+    {
+        if ($this->parameter->hasType()) {
+            $this->type = $this->parameter->getType();
+        }
+    }
+
+    /**
+     * Set the parameter.
+     *
+     * @return void
+     */
+    private function setReflectionParameter()
+    {
+        $this->parameters = $this->method->getParameters()[0];
+    }
+
+    /**
+     * Set the method.
+     *
+     * @param string $methodName
+     *
+     * @return $this
+     */
+    private function setReflectionMethod($methodName)
+    {
+        $methods = $this->class->getMethods();
+
+        $this->method = array_filter($methods, function ($method) {
+            return $method->name === $methodName;
+        });
+
+        return $this;
+    }
+
+    /**
+     * Set the class.
+     *
+     * @param string $class
+     *
+     * @return $this
+     */
+    private function setReflectionClass($class) {
+        $this->class = new \ReflectionClass(new $class());
+
+        return $this;
+    }
+
+    /**
+     * Resolve the class out of the Injector.
+     *
+     * @return object
+     */
     public function resolve()
     {
         $resolve = $this->type::from($this->httpParameters);
