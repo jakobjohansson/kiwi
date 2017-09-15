c8da5f104286523f22701d429ce548d2728a95fe
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-15 12:55:55 +0200

    add constructor

--- Core/System/Templating/Engine.php
+++ Core/System/Templating/Engine.php
@@ -4,5 +4,15 @@ namespace kiwi\System\Templating;
 
 class Engine
 {
-    
+    /**
+     * The file to interpret.
+     *
+     * @var string
+     */
+    private $file;
+
+    public function __construct($file)
+    {
+        $this->file = $file;
+    }
 }
