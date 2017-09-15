6b450c8943195873f2886fd27292d0202c61beac
Jakob Johansson <jakobjohansson2@icloud.com>
2017-09-15 13:29:04 +0200

    add an expressionCompiler

---
+++ Core/System/Templating/Compilers/ExpressionCompiler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace kiwi\System\Templating\Compilers;
+
+class ExpressionCompiler
+{
+    /**
+     * The regular expression to look for.
+     *
+     * @var string
+     */
+    private $expression;
+
+    /**
+     * The content to compile.
+     *
+     * @var string
+     */
+    private $content;
+
+    /**
+     * Create a new ExpressionCompiler instance.
+     *
+     * @param string $content
+     */
+    public function __construct($content)
+    {
+        $this->content = $content;
+
+        $this->setExpression();
+    }
+
+    /**
+     * Compile the content.
+     *
+     * @return void
+     */
+    public function compile()
+    {
+    }
+
+    /**
+     * Set the expression.
+     *
+     * @return void
+     */
+    private function setExpression()
+    {
+        $this->expression = "";
+    }
+}
