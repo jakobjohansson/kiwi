ed2cbe73a84b0e251cfd634fcc33f94dbe1d2a5a
Jakob Johansson <jakobjohansson2@icloud.com>
2017-10-03 16:53:58 +0200

    add digits rule

--- framework/Validation/Rule.php
+++ framework/Validation/Rule.php
@@ -5,32 +5,6 @@ namespace kiwi\Http;
 class Rule
 {
     /**
-     * Set a minimum string length rule.
-     *
-     * @param string $string
-     * @param int    $offset
-     *
-     * @return bool
-     */
-    public static function min($string, $offset)
-    {
-        return strlen($string) >= $offset;
-    }
-
-    /**
-     * Set a maximum string length rule.
-     *
-     * @param string $string
-     * @param int    $offset
-     *
-     * @return bool
-     */
-    public static function max($string, $offset)
-    {
-        return strlen($string) <= $offset;
-    }
-
-    /**
      * Set a required rule.
      *
      * @param string $string
@@ -66,6 +40,32 @@ class Rule
     }
 
     /**
+     * Set a minimum string length rule.
+     *
+     * @param string $string
+     * @param int    $offset
+     *
+     * @return bool
+     */
+    public static function min($string, $offset)
+    {
+        return strlen($string) >= $offset;
+    }
+
+    /**
+     * Set a maximum string length rule.
+     *
+     * @param string $string
+     * @param int    $offset
+     *
+     * @return bool
+     */
+    public static function max($string, $offset)
+    {
+        return strlen($string) <= $offset;
+    }
+
+    /**
      * Set a alphabetical only rule.
      *
      * @param  string $string
@@ -74,5 +74,16 @@ class Rule
     public static function alpha($string)
     {
         return preg_match('/^[A-Za-zÅÄÖåäö]+$/', $string) === true;
+    }
+
+    /**
+     * Set a digits only rule.
+     *
+     * @param  string $string
+     * @return bool
+     */
+    public static function digits($string)
+    {
+        return preg_match('/^\d+$/', $string) === true;
     }
 }
